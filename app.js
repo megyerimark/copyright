b3feafd284b96d0328502ffe41941fecedfb49a0
unknown <megyko1234@gmail.com>
2022-06-13 11:09:17 +0200

    Végeleges de nem jó copyright pepsi

---
+++ app.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.gold').src=anything;
+}
+function chaneBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.backgroung= color;
+}
